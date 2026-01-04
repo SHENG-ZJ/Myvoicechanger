c42a546ba1436a78662a7c31f04cd3de02e47d61
SHENG-ZJ <ShengZijie001@gmail.com>
2026-01-04 14:12:15 +0100

    Update build.py

--- build.py
+++ build.py
@@ -3,18 +3,18 @@ import customtkinter
 import os
 import platform
 
-# 1. 获取 customtkinter 库的安装路径 (自动寻找，无需手动查)
+# 获取 ctk 路径
 ctk_path = os.path.dirname(customtkinter.__file__)
-
-# 2. 确定分隔符 (Windows用分号, Mac/Linux用冒号)
 sep = ';' if platform.system() == "Windows" else ':'
 
-# 3. 运行 PyInstaller
 PyInstaller.__main__.run([
-    'main.py',                       # 你的主程序文件名 (如果是其他名字请修改这里)
-    '--name=小琛哥变声器',           # 生成的软件名字
-    '--noconsole',                   # 不显示黑色命令行窗口
-    '--onefile',                     # 打包成单个文件 (由文件夹变成一个独立程序)
-    '--clean',                       # 清理缓存
-    f'--add-data={ctk_path}{sep}customtkinter', # 关键：把 ctk 的资源文件塞进去
+    'main.py',
+    '--name=VoiceChanger',
+    '--noconsole',
+    '--onedir',   # <--- 关键修改：改为 onedir (文件夹模式)
+    '--clean',
+    f'--add-data={ctk_path}{sep}customtkinter',
+    # 强制让 macOS 把依赖打进去
+    '--collect-all=pedalboard', 
+    '--collect-all=sounddevice'
 ])
